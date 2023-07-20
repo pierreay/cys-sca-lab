d164725e332d695737711274d0d73d5000103cf7
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-07-20 11:58:10 +0200

    Look at power and em traces

--- expe.py
+++ expe.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
+import collections
+import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = (10, 4)
+
 def expe1():
-    pass
+    full_trace_power = np.load("data/02_full_trace_example.npy")
+    plt.plot(full_trace_power)
+    plt.xlabel("Sample")
+    plt.ylabel("Scope ADC value")
+    plt.title("Example Power Trace")
+    plt.show()
+
+    full_trace_em = np.load("data/02_full_trace_example_em.npy")
+    plt.plot(full_trace_em)
+    plt.xlabel("Sample")
+    plt.ylabel("Scope ADC value")
+    plt.title("Example EM Trace")
+    plt.show()
 
 if __name__ == "__main__":
     expe1()
